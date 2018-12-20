bda827d614340e8773fcc341b0b9f9410c7a1a15
Shaw <surashaw@amazon.com>
2018-12-20 14:55:45 +0530

    a

---
+++ CSS and Javascript/assignment.js
@@ -0,0 +1,7 @@
+
+function getdetails() {
+ document.getElementById("demo").innerHTML = "Changed to JS.";
+ /*alert("Please click OK");*/
+/* console.log("Check In Insprct Section");*/
+ document.write("Using this function previous data will be erased");
+}
