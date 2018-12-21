af4be14d867d2c722830bd1a50bfad540ee1e56a
Shaw <surashaw@amazon.com>
2018-12-21 14:37:04 +0530

    a

---
+++ CSS and Javascript/string position.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body> 
+<script>
+var str = "HELLO WORLD";
+document.getElementById("demo").innerHTML = str.charAt(0);
+</script>
+<button type="button" onclick="getdetails()">Click here</button>
+</body>
+</html>
